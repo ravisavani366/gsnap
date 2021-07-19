1b64b5bca82f832f6978d35bff74c7c8782e79d2
Ravi Savani <ravisavani366@gmail.com>
2021-07-19 17:06:28 +0530

    Update Welcome.js

--- gsnap/src/Components/Welcome/Welcome.js
+++ gsnap/src/Components/Welcome/Welcome.js
@@ -12,8 +12,9 @@ class Welcome extends React.Component{
                 src={this.props.currentUserPhoto ? this.props.currentUserPhoto : "https://firebasestorage.googleapis.com/v0/b/react-applciation.appspot.com/o/nopic.jpg?alt=media&token=d375ecd2-5905-4dee-8185-b351701915d0" }
                 alt=""
                 />
-                <span className="textTitleWelcome">{`Welcome,${this.props.currentUserName}` }</span>
-                <span className="textDescriptionWelcome">Start Chatting</span>
+                <span className="textTitleWelcome">{`Hello, ${this.props.currentUserName} 👋` }</span>
+                <span className="textDescriptionWelcome">Welcome to the Shree Diamond World 💎</span>
+                <marquee className="textDescriptionWelcome"><strong>⚠ Note:</strong> Please 🔖 Update your Daily Work Status and This is a Business diamond Chat do not use 🚫 for Personal</marquee>
             </div>
         )
     }
